e3c02887c6706740c7e3035458ec533d38348c99
Alper Uyar <alperuyar@192.168.1.34>
2022-08-23 14:42:10 +0300

    well.done

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const button = document.getElementById("result-submit");
+const BMI_Text = document.getElementById("BMI");
+const result_Text = document.getElementById("result");
+BMI_Text.style.display = "none";
+result_Text.style.display = "none";
 function loadBMI() {
   const weightValue = document.getElementById("weight").value;
   const heightValue = document.getElementById("height").value;
   const BMICalculation = weightValue / Math.pow(heightValue, 2);
-  const BMI_Text = document.getElementById("BMI");
+
   BMI_Text.innerHTML = "Your BMI is " + BMICalculation.toFixed(2);
-  const result_Text = document.getElementById("result");
+  BMI_Text.style.display = "";
+  result_Text.style.display = "";
   if (BMICalculation < 18.5) {
     result_Text.innerHTML = "Underweight";
   } else if (BMICalculation < 25) {
